73336b089ad8bf5bc42d48e9d8795c4656bccc16
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-18 18:19:41 +0100

    using hero animation to transition from our welcome screen to the login and registration screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(FlashChat());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => FlashChat(), // Wrap your app
+      ),
+    );
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           bodyText1: TextStyle(color: Colors.black54),
