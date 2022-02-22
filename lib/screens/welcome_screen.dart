75d1067effa07caaf175fbea0c89b81becc3f3df
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-22 11:46:26 +0100

    using flutter animation packages - animated text kit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String welcome_id = 'welcome_screen';
@@ -60,11 +61,13 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
+                DefaultTextStyle(
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [TypewriterAnimatedText('Flash Chat')],
                   ),
                 ),
               ],
