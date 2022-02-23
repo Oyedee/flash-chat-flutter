8672a9ef2e87a07466febeb2d93c0947250ce34f
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-02-23 10:20:15 +0100

    listening for data from firebase using streams

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -32,6 +33,20 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  /*void getMessages() async {
+    final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data());
+    }
+  }*/
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots())
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +57,8 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
-                _auth.signOut();
-                Navigator.pop(context);
+                /*_auth.signOut();
+                Navigator.pop(context);*/
               }),
         ],
         title: Text('⚡️Chat'),
